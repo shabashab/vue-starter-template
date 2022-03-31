7461c62ebdd4b9bc70cba363f462f3c242d3c7b9
Artem Tarasenko <shabashab.04@gmail.com>
2022-03-31 21:04:45 +0300

    feat: add auto import

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { useHead } from "@vueuse/head";
-
 useHead({
   title: "Vue Starter Template",
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
 import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,10 @@ export default defineConfig({
       extensions: ["vue"],
       include: [/\.vue$/, /\.vue\?vue/],
       dts: "src/components.d.ts",
+    }),
+    AutoImport({
+      dts: "src/auto-import.d.ts",
+      imports: ["vue", "@vueuse/head"],
     }),
   ],
 });
