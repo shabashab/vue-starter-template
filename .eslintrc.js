04fd9d928a9b82dd1c8779b1ec9153d4964b247c
Artem Tarasenko <shabashab.04@gmail.com>
2022-04-01 17:18:41 +0300

    feat: replace prettier with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  rules: {
+    quotes: "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-undef": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/quotes": ["error", "double"],
+    "vue/multi-word-component-names": ["error"],
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,27 @@ const onButtonClick = () => {
 };
 
 const buttonText = computed(() => {
-  return isDark.value ? "Enter light mode": "Enter dark mode";
+  return isDark.value ? "Enter light mode" : "Enter dark mode";
 });
 </script>
 
 <template>
   <div
-    class="bg-white dark:bg-dark-900 w-screen h-screen flex flex-col justify-center items-center text-dark-200 dark:text-white"
+    class="bg-white dark:bg-dark-900 text-dark-200 dark:text-white w-screen h-screen flex flex-col justify-center items-center"
   >
-    <h1 class="font-bold font-primary prose-2xl">Vue Starter Template</h1>
+    <h1 class="font-bold font-primary prose-2xl">
+      Vue Starter Template
+    </h1>
     <p class="prose font-secondary text-center mt-10">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet possimus
       reprehenderit dolore aliquid voluptates quos beatae perspiciatis rerum,
       deserunt similique corporis culpa enim repellendus ipsa obcaecati iusto
       vitae voluptate sed!
     </p>
-    <Button @click="onButtonClick" class="mt-5" :text="buttonText" />
+    <PageButton
+      class="mt-5"
+      :text="buttonText"
+      @click="onButtonClick"
+    />
   </div>
 </template>
